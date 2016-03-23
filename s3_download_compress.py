b8c6ed00a816691338914fc58ec72302c92d485e
Kevin Coakley <kcoakley@sdsc.edu>
2016-03-23 10:46:47 -0700

    If the file already exists then skip it

--- s3_download_compress.py
+++ s3_download_compress.py
@@ -1,25 +1,34 @@
-import boto3
 import gzip
+import boto3
+import os.path
 
 count = 1
 
 while count < 100:
-    print "Start: data/election%03d" % count
 
-    try:
-        key = boto3.resource('s3').Object("ucsd-twitter",
-                                          "data/election%03d" % count).get(RequestPayer='requester')
+    save_path = "/save/election%03d.gz" % count
 
-        with gzip.open("/save/election%03d.gz" % count, 'w') as f:
-            chunk = key['Body'].read(1024*8)
-            while chunk:
-                f.write(chunk)
+    if not os.path.exists(save_path):
+
+        print "Start: data/election%03d" % count
+
+        try:
+            key = boto3.resource('s3').Object("ucsd-twitter", "data/election%03d" % count)\
+                .get(RequestPayer='requester')
+
+            with gzip.open(save_path, 'w') as f:
                 chunk = key['Body'].read(1024*8)
+                while chunk:
+                    f.write(chunk)
+                    chunk = key['Body'].read(1024*8)
 
-        f.close()
-    except Exception as e:
-        print "Exception: %s" % e
+            f.close()
+        except Exception as e:
+            print "Exception: %s" % e
 
-    print "End: data/election%03d\n" % count
+        print "End: data/election%03d\n" % count
+
+    else:
+        print "%s exists, skipping!" % save_path
 
     count += 1
