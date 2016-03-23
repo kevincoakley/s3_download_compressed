20448e7f630b7bf4f30ec1dc53a889f7ac1c867b
Kevin Coakley <kcoakley@sdsc.edu>
2016-03-23 10:09:20 -0700

    Download text file from s3 and save to a compressed gz file

---
+++ s3_download_compress.py
@@ -0,0 +1,25 @@
+import boto3
+import gzip
+
+count = 0
+
+while count < 100:
+    print "Start: data/election%03d" % count
+
+    try:
+        key = boto3.resource('s3').Object("ucsd-twitter",
+                                          "data/election%03d" % count).get(RequestPayer='requester')
+
+        with gzip.open("/save/election%03d.gz" % count, 'w') as f:
+            chunk = key['Body'].read(1024*8)
+            while chunk:
+                f.write(chunk)
+                chunk = key['Body'].read(1024*8)
+
+        f.close()
+    except Exception as e:
+        print "Exception: %s" % e
+
+    print "End: data/election%03d\n" % count
+
+    count += 1
